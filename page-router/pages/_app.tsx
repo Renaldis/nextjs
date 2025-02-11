8631e0aab6eb143ceaed8aa4092c94966ece6d58
Renaldis <renaldis142@gmail.com>
2025-02-11 21:44:41 +0700

    add tailwindcss for styling, create RootLayoutComponent

--- page-router/pages/_app.tsx
+++ page-router/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import RootLayout from "@/layout";
+import { useRouter } from "next/router";
+// import { useRouter } from "next/navigation";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const metaTitle =
+    router.pathname === "/" ? "Home" : router.pathname.replace("/", "");
+
+  console.log(router);
+  return (
+    <>
+      <RootLayout metaTitle={metaTitle}>
+        <Component {...pageProps} />
+      </RootLayout>
+    </>
+  );
 }
