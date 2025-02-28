3cddfd51b47c19058ba8d877b97d9d74597a980c
Renaldis <renaldis142@gmail.com>
2025-02-28 16:52:39 +0700

    part 1

--- nexts-fullstack/pages/index.tsx
+++ nexts-fullstack/pages/index.tsx
@@ -3,7 +3,6 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -18,54 +17,13 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-
-const formSchema = z.object({
-  title: z.string().min(1),
-  url: z.string().min(1),
-});
+import FormContainer from "@/container/FormContainer";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 export default function Home() {
   const { data: dataLinks, error, isLoading } = useSWR("/api/links", fetcher);
-
-  const [loading, setLoading] = useState<boolean>(false);
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      url: "",
-    },
-  });
-
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    setLoading(true);
-    console.log(values);
-    try {
-      const response = await fetch("/api/links/create", {
-        method: "POST",
-        body: JSON.stringify(values),
-      });
-    } catch (error) {
-    } finally {
-      setLoading(false);
-    }
-  }
 
   return (
     <div className="grid grid-cols-1 gap-4">
@@ -75,39 +33,7 @@ export default function Home() {
           <CardDescription>Submit your link here</CardDescription>
         </CardHeader>
         <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Title</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Title ..." {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="url"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Url</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Url ..." {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit" disabled={loading}>
-                {loading ? "Loading..." : "Submit"}
-              </Button>
-            </form>
-          </Form>
+          <FormContainer />
         </CardContent>
       </Card>
       {isLoading && <p>Loading ...</p>}
@@ -146,9 +72,6 @@ export default function Home() {
           </Card>
         )
       )}
-      <Card>
-        <CardContent></CardContent>
-      </Card>
     </div>
   );
 }
