4aa8110a758f38d2e341f107b18077afe53260bc
Renaldis <renaldis142@gmail.com>
2025-02-28 17:15:40 +0700

    part 2

--- nexts-fullstack/pages/index.tsx
+++ nexts-fullstack/pages/index.tsx
@@ -18,60 +18,105 @@ import {
 } from "@/components/ui/drawer";
 
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import FormContainer from "@/container/FormContainer";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 export default function Home() {
-  const { data: dataLinks, error, isLoading } = useSWR("/api/links", fetcher);
+  const popoverRef = useRef<HTMLButtonElement | null>(null);
+  const [showCreate, setShowCreate] = useState<boolean>(false);
+  const [showEdit, setShowEdit] = useState<boolean>(false);
+  const [valueEdit, setShowValueEdit] = useState<{
+    id: number;
+    title: string;
+    url: string;
+  }>({
+    id: 0,
+    title: "",
+    url: "",
+  });
+  const {
+    data: dataLinks,
+    error,
+    isLoading,
+    mutate,
+  } = useSWR("/api/links", fetcher);
+
+  const handleDelete = async (id: number) => {
+    try {
+      await fetch(`/api/links/delete/${id}`, {
+        method: "DELETE",
+      });
+    } catch (error) {
+    } finally {
+      mutate();
+      popoverRef.current?.click();
+    }
+  };
 
   return (
-    <div className="grid grid-cols-1 gap-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Form Create Link</CardTitle>
-          <CardDescription>Submit your link here</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <FormContainer />
-        </CardContent>
-      </Card>
-      {isLoading && <p>Loading ...</p>}
-      {dataLinks?.data?.map(
-        (link: { id: number; title: string; url: string }) => (
-          <Card key={link.id}>
-            <CardContent className="flex justify-between items-center">
-              <a
-                href={link.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label={link.title}
-              >
-                {link.title}
-              </a>
-              <Drawer>
-                <DrawerTrigger asChild>
-                  <Button variant="outline">Edit</Button>
-                </DrawerTrigger>
-                <DrawerContent>
-                  <DrawerHeader>
-                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-                    <DrawerDescription>
-                      This action cannot be undone.
-                    </DrawerDescription>
-                  </DrawerHeader>
-                  <DrawerFooter>
-                    <Button>Submit</Button>
-                    <DrawerClose asChild>
-                      <Button variant="outline">Cancel</Button>
-                    </DrawerClose>
-                  </DrawerFooter>
-                </DrawerContent>
-              </Drawer>
-            </CardContent>
-          </Card>
-        )
-      )}
-    </div>
+    <>
+      <div className="grid grid-cols-1 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Form Create Link</CardTitle>
+            <CardDescription>Submit your link here</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <FormContainer />
+          </CardContent>
+        </Card>
+        {isLoading && <p>Loading ...</p>}
+        {dataLinks?.data?.map(
+          (link: { id: number; title: string; url: string }) => (
+            <Card key={link.id}>
+              <CardContent className="flex justify-between items-center">
+                <a
+                  href={link.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label={link.title}
+                >
+                  {link.title}
+                </a>
+
+                <Button
+                  size="sm"
+                  variant="secondary"
+                  onClick={() => {
+                    setShowValueEdit({
+                      id: link.id,
+                      title: link.title,
+                      url: link.url,
+                    });
+                    setShowEdit(true);
+                  }}
+                >
+                  Edit
+                </Button>
+              </CardContent>
+            </Card>
+          )
+        )}
+      </div>
+
+      <Drawer open={showEdit} onOpenChange={setShowEdit}>
+        <DrawerContent>
+          <div className="container mx-auto p-4">
+            <FormContainer
+              id={valueEdit.id}
+              values={{
+                title: valueEdit.title,
+                url: valueEdit.url,
+              }}
+              onFinished={() => {
+                setShowEdit(false);
+                mutate();
+              }}
+            />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
